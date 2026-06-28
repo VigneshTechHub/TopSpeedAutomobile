e9a9b3684e08fbbf005528ccb06cb438cc80fab3
VigneshTechHub <vigneshravicoder@gmail.com>
2026-06-28 09:34:08 +0530

    code changes

---
+++ TopSpeed.Domain/Models/VickyModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TopSpeed.Domain.Models
+{
+    internal class VickyModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string Age { get; set; }
+
+    }
+}
